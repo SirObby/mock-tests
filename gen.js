7ca91d4742d3600b3ebc517b1ed33dbff775efe7
SirObby <70753342+SirObby@users.noreply.github.com>
2022-05-17 21:02:17 +0800

    php?

--- gen.js
+++ gen.js
@@ -1,3 +1,54 @@
+let found_body = {};
+
+function generate_page() {
+    /*
+    <textarea id="input" rows="10" cols="50" placeholder="Insert test data information here."></textarea>
+    <br>
+    */
+    document.getElementById("entire-page").innerHTML = `
+    <button id="start-btn" onclick="start()">Start mock</button>
+    <div id="exam-area">
+
+    </div>
+    <button id="check_answers" onclick="javascript:submit();" disabled>Check answers.</button>
+    <button id="submit_answers" onclick="javascript:send();" disabled>Submit answers.</button> <span data-toggle='tooltip' title='This will email the teacher with your answers.'>?</span>
+
+    <div id="mark_count">
+
+    </div>`
+
+    document.getElementById("check_answers").disabled = true;
+    document.getElementById("submit_answers").disabled = true;
+
+    //document.getElementById("input").value = found_body.body;
+}
+
+function get_mock() {
+    // GET get.php/?id=${document.getElementById("id").value}
+    // And set found_body to the returned JSON object
+    // If nothing is returned set found_body to null
+    // If something is returned, set found_body to the returned JSON object
+    console.log(`get.php?id=${document.getElementById("id").value}`);
+    fetch(`get.php?id=${document.getElementById("id").value}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            found_body = data;
+            
+            setTimeout(() => {
+                generate_page();
+            }, 100);
+            //
+        })
+        .catch(error => { console.error(error); console.log(response)});
+
+}
+
 let answered = {
 
 }
@@ -6,43 +57,40 @@ let correct_answers = {
 }
 
 let timer = 0;
-let timer_enabled = false; 
+let timer_enabled = false;
 
 let marks_awarded = 0;
 let marks_achieved = 0;
 
 let q = 0; // How many questions?
 
-document.getElementById("check_answers").disabled = true;
-document.getElementById("submit_answers").disabled = true;
-
 function start() {
-    let code = atob(document.getElementById("input").value);
+    let code = /*document.getElementById("input").value*/found_body.body.toString();
 
     console.log(code);
 
     // Parse code into a JSON object
     let parsed = JSON.parse(code);
 
-    document.getElementById("input").remove();
+    //document.getElementById("input").remove();
     document.getElementById("start-btn").remove();
 
     let result = "";
 
     for (let index = 0; index < parsed["questions"].length; index++) {
         const element = parsed["questions"][index];
-        
+
         console.log(element.question)
 
         let choices = "";
 
         for (let indexy = 0; indexy < element["c"]; indexy++) {
             const elementt = element["choices"][indexy];
-            
+
             console.log(element.text);
             choices += `<button class="choice_btn" onclick="select_choice(${index}, ${indexy})"><b>${indexy + 1}.</b> <input type="checkbox" id="c-${index}-${indexy}" tabindex="-1"> ${elementt.text}</button><br>`
-        
-            if(elementt.correct == true) {
+
+            if (elementt.correct == true) {
                 correct_answers[index] = indexy;
             }
         }
@@ -57,7 +105,7 @@ function start() {
 
     document.getElementById("check_answers").disabled = false;
     //document.getElementById("check_answers").onclick = "javascript:submit();"
-    if(parsed.submit) {
+    if (parsed.submit) {
         document.getElementById("submit_answers").disabled = false;
         //document.getElementById("submit_answers").onclick = "javascript:send();"
     }
@@ -73,18 +121,18 @@ function select_choice(q, c) {
     for (let index = 0; index < 3; index++) {
         const element = document.getElementById(`c-${q}-${index}`);
 
-        if(element.checked == true) {
+        if (element.checked == true) {
             element.checked = false;
-        } 
+        }
     }
     checkbox.checked = true;
-    
+
     answered[q] = c;
 }
 
 function submit() {
     for (let index = 0; index < q; index++) {
-        if(answered[index] == correct_answers[index]) {
+        if (answered[index] == correct_answers[index]) {
             marks_achieved++;
             document.getElementById(`${index}-corr`).innerHTML = "Correct";
         } else {
@@ -94,6 +142,7 @@ function submit() {
 
     document.getElementById("mark_count").innerHTML = `You achived ${marks_achieved} out of ${marks_awarded} marks.`;
 }
+
 function send() {
     let body = ``;
 }
